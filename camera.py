1aa60dbb09c1220f75ccb6450ce23e9c835d2e83
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2025-05-13 14:32:46 +0200

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,5 +1,3 @@
-import cv2
-import numpy as np
 import urllib.request
 
 class VideoCamera(object):
@@ -20,17 +18,8 @@ class VideoCamera(object):
 
                 if start != -1 and end != -1 and end > start:
                     jpg = self.bytes[start:end+2]
-                    self.bytes = self.bytes[end+2:]  # Rest behalten, evtl. nächstes Bild schon teilweise da
-                    img = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-                    if img is not None:
-                        resized_image = cv2.resize(img, (160, 120))
-                        ret, jpeg = cv2.imencode('.jpg', resized_image)
-                        return jpeg.tobytes()
-                    else:
-                        print("⚠️ Fehler beim Dekodieren des Bildes.")
-                        return b''
-                # Andernfalls einfach weiter lesen bis genug Daten da sind
-
+                    self.bytes = self.bytes[end+2:]  # Buffer aufräumen
+                    return jpg  # ? Direkt das JPEG-Bytearray zurückgeben ? ohne Modifikation
         except Exception as e:
-            print(f"⚠️ Fehler beim Lesen des MJPEG Streams: {e}")
+            print(f"?? Fehler beim Lesen des MJPEG Streams: {e}")
             return b''
