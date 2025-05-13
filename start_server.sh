545aa1eb605543495c3df1775d20894a183359c9
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2025-05-13 12:12:26 +0200

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,8 +1,8 @@
-gh pull
+git pull
 # install dependencies
-./venv/bin/pip3 install -r requirements.txt
+./.venv/bin/pip3 install -r requirements.txt
 # Start the server in the background (main.py)
-./venv/bin/python3 main.py &
+./.venv/bin/python3 main.py &
 # Get the PID of the last background process
 pid=$!
 echo "Server started with PID: $pid"
