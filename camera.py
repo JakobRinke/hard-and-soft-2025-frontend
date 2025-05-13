20c97b6ba417a3c3b7bcffb0e8ca597c813e17cb
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2025-05-13 13:04:50 +0200

    Update camera.py

--- camera.py
+++ camera.py
@@ -2,7 +2,7 @@ import cv2
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(0)
+        self.video = cv2.VideoCapture("dev/video0")
 
 
     def __del__(self):
