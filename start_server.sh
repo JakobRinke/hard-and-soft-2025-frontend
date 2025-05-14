0a9d88a30af3aa00d498c3eaaf7c5193768bb047
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2025-05-14 15:34:10 +0200

    d

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,6 @@
-git pull
+git pull || true
 # install dependencies
-./.venv/bin/pip3 install -r requirements.txt
+./.venv/bin/pip3 install -r requirements.txt > NUL || true 
 # Start the server in the background (main.py)
 #./.venv/bin/python3 app.py &
 ./.venv/bin/python3 app.py 
